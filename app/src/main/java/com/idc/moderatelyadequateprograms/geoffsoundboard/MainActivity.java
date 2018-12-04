6f80bc109d2fda2f4a194045f94f54feec73e2d7
ntsmith24 <kooln8@gmail.com>
2018-12-04 14:50:06 -0600

    More sounds

--- app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
+++ app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
@@ -7,17 +7,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    //private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = findViewById(R.id.message);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.wave);
+        //mTextMessage = findViewById(R.id.message);
+        FloatingActionButton fab =  findViewById(R.id.wave);
         /*
         Sound events
          */
@@ -28,14 +28,16 @@ public class MainActivity extends AppCompatActivity {
         final MediaPlayer welcomeToCollegeSound = MediaPlayer.create(this, R.raw.welcome_to_college);
         final MediaPlayer welcomeToUIUCSound = MediaPlayer.create(this, R.raw.welcome_to_uiuc);
         final MediaPlayer alright1Sound = MediaPlayer.create(this, R.raw.alright);
+        final MediaPlayer iSeeOnePersonSound = MediaPlayer.create(this, R.raw.i_see_one_person);
 
-        final Button goodMorning = (Button) this.findViewById(R.id.goodMorning);
-        final Button welcomeToCs125 = (Button) this.findViewById(R.id.welcomeToCs125);
-        final Button myNameIsGeoffChallen = (Button) this.findViewById(R.id.myNameIsGeoffChallen);
-        final Button roundOfApplause = (Button) this.findViewById(R.id.roundOfApplause);
-        final Button welcomeToCollege = (Button) this.findViewById(R.id.welcomeToCollege);
-        final Button welcomeToUIUC = (Button) this.findViewById(R.id.welcomeToUIUC);
-        final Button alright = (Button) this.findViewById(R.id.alright);
+        final Button goodMorning = this.findViewById(R.id.goodMorning);
+        final Button welcomeToCs125 = this.findViewById(R.id.welcomeToCs125);
+        final Button myNameIsGeoffChallen = this.findViewById(R.id.myNameIsGeoffChallen);
+        final Button roundOfApplause = this.findViewById(R.id.roundOfApplause);
+        final Button welcomeToCollege = this.findViewById(R.id.welcomeToCollege);
+        final Button welcomeToUIUC = this.findViewById(R.id.welcomeToUIUC);
+        final Button alright = this.findViewById(R.id.alright);
+        final Button iSeeOnePerson = this.findViewById(R.id.iSeeOnePerson);
 
         goodMorning.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +81,16 @@ public class MainActivity extends AppCompatActivity {
                 alright1Sound.start();
             }
         });
+        iSeeOnePerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iSeeOnePersonSound.start();
+            }
+        });
+
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
