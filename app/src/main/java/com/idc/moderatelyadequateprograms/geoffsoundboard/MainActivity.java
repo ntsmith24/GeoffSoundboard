4c210b5f8e16fc6c8b4d515d6d0d7e3f5ba77151
ntsmith24 <kooln8@gmail.com>
2018-12-04 12:08:49 -0600

    Added FAB and fixed wave activity

--- app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
+++ app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
@@ -3,42 +3,31 @@ package com.idc.moderatelyadequateprograms.geoffsoundboard;
 import android.content.Intent;
 import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.wave);
-                    Intent wave = new Intent(MainActivity.this, WaveActivity.class);
-                    startActivity(wave);
-                    return true;
-            }
-            return false;
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        mTextMessage = findViewById(R.id.message);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.wave);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, WaveActivity.class));
+            }
+        });
     }
+
+
+
+
 
 
 }
--- app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/WaveActivity.java
+++ app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/WaveActivity.java
@@ -1,43 +1,28 @@
 package com.idc.moderatelyadequateprograms.geoffsoundboard;
 
-import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
-import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class WaveActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    Intent home = new Intent(WaveActivity.this, MainActivity.class);
-                    startActivity(home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.wave);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wave);
-        mTextMessage = findViewById(R.id.textView);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.home);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
 }
