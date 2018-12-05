6114c3887473d79323823cc251f9988f8ed611a8
ntsmith24 <kooln8@gmail.com>
2018-12-04 19:46:16 -0600

    Added a lot of buttons

--- app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
+++ app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/MainActivity.java
@@ -27,8 +27,22 @@ public class MainActivity extends AppCompatActivity {
         final MediaPlayer roundOfApplauseSound = MediaPlayer.create(this, R.raw.round_of_applause);
         final MediaPlayer welcomeToCollegeSound = MediaPlayer.create(this, R.raw.welcome_to_college);
         final MediaPlayer welcomeToUIUCSound = MediaPlayer.create(this, R.raw.welcome_to_uiuc);
-        final MediaPlayer alright1Sound = MediaPlayer.create(this, R.raw.alright);
+        final MediaPlayer alrightSound = MediaPlayer.create(this, R.raw.alright);
         final MediaPlayer iSeeOnePersonSound = MediaPlayer.create(this, R.raw.i_see_one_person);
+        final MediaPlayer alright1Sound = MediaPlayer.create(this, R.raw.alright1);
+        final MediaPlayer alright2Sound = MediaPlayer.create(this, R.raw.alright2);
+        final MediaPlayer coolSound = MediaPlayer.create(this, R.raw.cool);
+        final MediaPlayer spookySound = MediaPlayer.create(this, R.raw.spooky);
+        final MediaPlayer spookyMusicSound = MediaPlayer.create(this, R.raw.spooky_music);
+        final MediaPlayer halloweenSound = MediaPlayer.create(this, R.raw.welcome_to_halloween);
+        final MediaPlayer treesSound = MediaPlayer.create(this, R.raw.trees);
+        final MediaPlayer niceSound = MediaPlayer.create(this, R.raw.nice);
+        final MediaPlayer soSound = MediaPlayer.create(this, R.raw.so);
+        final MediaPlayer okSound = MediaPlayer.create(this, R.raw.ok);
+        final MediaPlayer um0Sound = MediaPlayer.create(this, R.raw.um);
+        final MediaPlayer um1Sound = MediaPlayer.create(this, R.raw.um1);
+        final MediaPlayer youKnowSound = MediaPlayer.create(this, R.raw.you_know);
+        final MediaPlayer todaySound = MediaPlayer.create(this, R.raw.today);
 
         final Button goodMorning = this.findViewById(R.id.goodMorning);
         final Button welcomeToCs125 = this.findViewById(R.id.welcomeToCs125);
@@ -38,6 +52,20 @@ public class MainActivity extends AppCompatActivity {
         final Button welcomeToUIUC = this.findViewById(R.id.welcomeToUIUC);
         final Button alright = this.findViewById(R.id.alright);
         final Button iSeeOnePerson = this.findViewById(R.id.iSeeOnePerson);
+        final Button alright1 = this.findViewById(R.id.alright1);
+        final Button alright2 = this.findViewById(R.id.alright2);
+        final Button cool = this.findViewById(R.id.cool);
+        final Button spooky = this.findViewById(R.id.spooky);
+        final Button spookyMusic = this.findViewById(R.id.spookyMusic);
+        final Button halloween = this.findViewById(R.id.welcomeToHalloween);
+        final Button trees = this.findViewById(R.id.trees);
+        final Button nice = this.findViewById(R.id.nice);
+        final Button so = this.findViewById(R.id.so);
+        final Button ok = this.findViewById(R.id.ok);
+        final Button um0 = this.findViewById(R.id.um0);
+        final Button um1 = this.findViewById(R.id.um1);
+        final Button youKnow = this.findViewById(R.id.youKnow);
+        final Button today = this.findViewById(R.id.today);
 
         goodMorning.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         alright.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                alright1Sound.start();
+                alrightSound.start();
             }
         });
         iSeeOnePerson.setOnClickListener(new View.OnClickListener() {
@@ -87,9 +115,90 @@ public class MainActivity extends AppCompatActivity {
                 iSeeOnePersonSound.start();
             }
         });
-
-
-
+        alright1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alright1Sound.start();
+            }
+        });
+        alright2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alright2Sound.start();
+            }
+        });
+        cool.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                coolSound.start();
+            }
+        });
+        spooky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spookySound.start();
+            }
+        });
+        spookyMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spookyMusicSound.start();
+            }
+        });
+        halloween.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                halloweenSound.start();
+            }
+        });
+        trees.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                treesSound.start();
+            }
+        });
+        nice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                niceSound.start();
+            }
+        });
+        so.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soSound.start();
+            }
+        });
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                okSound.start();
+            }
+        });
+        um0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                um0Sound.start();
+            }
+        });
+        um1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                um1Sound.start();
+            }
+        });
+        youKnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youKnowSound.start();
+            }
+        });
+        today.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                todaySound.start();
+            }
+        });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
