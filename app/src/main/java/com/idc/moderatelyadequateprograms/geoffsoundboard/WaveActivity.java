9849f8fb5721b13da32b5689019b50bafaa5edf9
cgpelletier <colegp2@illinois.edu>
2018-12-10 17:11:54 -0600

    New Shake Detector. No sound implementation yet, but at least it works right?

--- app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/WaveActivity.java
+++ app/src/main/java/com/idc/moderatelyadequateprograms/geoffsoundboard/WaveActivity.java
@@ -1,11 +1,19 @@
 package com.idc.moderatelyadequateprograms.geoffsoundboard;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+import android.os.Handler;
 
 public class WaveActivity extends AppCompatActivity {
 
@@ -23,6 +31,65 @@ public class WaveActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        accel = 0.00f;
+        accelCurrent = SensorManager.GRAVITY_EARTH;
+        accelLast = SensorManager.GRAVITY_EARTH;
+
+        handler.post(runnableCode);
     }
+
+    Handler handler = new Handler();
+
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+
+            if (accel > 2) {
+                Log.i("SHAKE", "SHOOKETH");
+            }
+
+            handler.postDelayed(this, 500);
+        }
+    };
+
+
+    private SensorManager mSensorManager;
+    private float accel; // acceleration apart from gravity
+    private float accelCurrent; // current acceleration including gravity
+    private float accelLast; // last acceleration including gravity
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        public void onSensorChanged(SensorEvent sens) {
+            float x = sens.values[0];
+            float y = sens.values[1];
+            float z = sens.values[2];
+            accelLast = accelCurrent;
+            accelCurrent = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = accelCurrent - accelLast;
+            accel = accel * 0.9f + delta;
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.post(runnableCode);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        handler.removeCallbacks(runnableCode);
+        mSensorManager.unregisterListener(mSensorListener);
+        super.onPause();
+    }
+
 
 }
